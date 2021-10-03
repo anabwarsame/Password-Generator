1af681ada2a163ab241d961217feaa4b452857b8
anabwarsame <anab_1994@hotmail.co.uk>
2021-10-03 22:48:20 +0100

    password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,24 @@ function generatePassword() {
   const uppercase = “ABCDEFGHIJKLMNOPQRSTUVWXYZ”;
   const numeric = “0123456789";
   const special = “!@£$%&*?+=#():%:“
+
+  // get password length
+  const passwordLength = Number(prompt(“Enter password length:“));
+  // if password is a number and within range of 8 and 128 do rest else alert and return “”
+  console.log(passwordLength);
+  /*
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert(“Please choose a different length?“);
+  } else {
+    return “”;
+  }
+*/
+
+if (passwordLength<8 || passwordLength<= 128) {
+    alert( "please choose a different length");
+    return passwordLength;
+} else if (passwordLength>=8 || passwordLength<= 128) {
+
+}
+
+// functions to determine what character user wants to use
