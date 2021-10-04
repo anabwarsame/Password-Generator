8a618aa19861850dc828e3be802114f0768c6530
anabwarsame <anab_1994@hotmail.co.uk>
2021-10-04 10:58:49 +0100

    added changes to strings in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,38 @@
-var generateBtn = document.querySelector(“#generate”);
+var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // PLEASE ADD ALL YOUR CODE HERE
   //declare criteria as an object
-  const lowerCase = “abcdefghijklmnopqrstuvwxyz”;
-  const uppercase = “ABCDEFGHIJKLMNOPQRSTUVWXYZ”;
-  const numeric = “0123456789";
-  const special = “!@£$%&*?+=#():%:“
+
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numeric = "0123456789";
+  const special = ",./?<>!@£$%^&*()#=+-_";
 
   // get password length
-  const passwordLength = Number(prompt(“Enter password length:“));
+  const passwordLength = Number(prompt("enter password length"));
   // if password is a number and within range of 8 and 128 do rest else alert and return “”
   console.log(passwordLength);
-  /*
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    alert(“Please choose a different length?“);
-  } else {
-    return “”;
-  }
-*/
 
-if (passwordLength<8 || passwordLength<= 128) {
-    alert( "please choose a different length");
+  if (passwordLength < 8 || passwordLength <= 128) {
+    alert("please choose a different length");
     return passwordLength;
-} else if (passwordLength>=8 || passwordLength<= 128) {
+  } else if (passwordLength >= 8 || passwordLength <= 128) {
+  }
 
-}
-
-// functions to determine what character user wants to use
-const isLowercase = confirm(“Do you want to use lowercase?“);
-  const isUppercase = confirm(“Do you want to use uppercase?“);
-  const isNumeric = confirm(“Do you want to use numeric?“);
-  const isSpecial = confirm(“Do you want to use special?“);
+  // functions to determine what character user wants to use
+  const isLowercase = confirm("Do you want to use lowercase?");
+  const isUppercase = confirm("Do you want to use uppercase?");
+  const isNumeric = confirm("Do you want to use numeric?");
+  const isSpecial = confirm("Do you want to use special?");
 
   if (!isLowercase && !isUppercase && !isNumeric && !isSpecial) {
     alert("please choose atleast one of the options");
     return;
   }
 
-    //return function
+  //return function
   //if statement for return
-
 
   const choices = [];
 
@@ -73,8 +65,8 @@ const isLowercase = confirm(“Do you want to use lowercase?“);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector(“#password”);
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 // Add event listener to generate button
-generateBtn.addEventListener(“click”, writePassword);
+generateBtn.addEventListener("click", writePassword);
